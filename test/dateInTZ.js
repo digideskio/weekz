f5793d90a3b002f3dba4ee6ffebb4db2798ee5aa
Martin Heidegger <martin.heidegger@gmail.com>
2016-07-28 15:23:10 +0200

    Remove test that can't work.

--- test/dateInTZ.js
+++ test/dateInTZ.js
@@ -2,11 +2,6 @@
 const test = require('tap').test
 const dateInTZ = require('../dateInTZ.js')
 
-test('Get correct timezone offset', function (t) {
-  t.equal(dateInTZ(2015, 1, 10, '+0700').getTimezoneOffset(), -60)
-  t.end()
-})
-
 test('Make sure that times are correct', function (t) {
   t.equal(dateInTZ(2015, 1, 10, '+0700').toISOString(), '2015-02-09T17:00:00.000Z')
   t.end()
